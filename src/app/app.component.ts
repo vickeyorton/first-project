1100618421fc81b6031f1dfc81cc1c298c7489e6
vickeyorton <vigneshmooorthi@gmail.com>
2020-02-15 17:55:40 +0530

    Member Variables to Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './loop/user.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-project';
+  user:User;
+  constructor(){
+        this.user = new User();
+      
+        this.user.name = 'Vignesh';
+        this.user.title = 'Software Developer';
+        this.user.address = 'Ayanavaram, Chennai:600023, TamilNadu, India';
+        this.user.Phone = [
+          '+91 9962354801',
+          '+91 9962494801',
+          '+91 8778703972'
+        ];
+      
+  }
 }
--- src/app/loop/loop.component.ts
+++ src/app/loop/loop.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-loop',
@@ -7,21 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoopComponent implements OnInit {
 
-  user : any;
+  @Input('user') user:User;
   constructor() {
-    this.user = {
-      name : 'Vignesh',
-      title : 'Software Developer',
-      address : 'Ayanavaram, Chennai:600023, TamilNadu, India',
-      Phone : [
-        '+91 9962354801',
-        '+91 9962494801',
-        '+91 8778703972'
-      ]
-    };
+    
    }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.user = {
+      name : this.user.name,
+      title : this.user.title,
+      address : this.user.address,
+      Phone : this.user.Phone
+    };
   }
 
 }
